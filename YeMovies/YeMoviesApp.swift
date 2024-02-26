45adf784f200e98eb8025b99a72c8db2a278b846
Yegor Myropoltsev <72651640+yegormi@users.noreply.github.com>
2024-02-26 20:27:17 +0200

    Initial Commit

---
+++ YeMovies/YeMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  YeMoviesApp.swift
+//  YeMovies
+//
+//  Created by Yegor Myropoltsev on 26.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct YeMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
